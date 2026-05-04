d7f03025872fd044046e2cd2e6550959dbf9a95a
kopeika123 <3101000@bk.ru>
2026-05-04 09:15:32 +0500

    Add ESLint config for workspace linting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['node_modules/', 'dist/', '.turbo/', '.github/'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+};
